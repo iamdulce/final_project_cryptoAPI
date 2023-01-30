bd7abcfbbbeaf7a72256127836628c7e0465b928
unknown <dulce.1201@gmail.com>
2023-01-30 20:19:22 +0100

    First try calling API and use modifying the Purchase page to get the data from the form

--- crypto_app/models.py
+++ crypto_app/models.py
@@ -1,3 +1,51 @@
-import sqlite3
 import requests
+#import sqlite3
+#from crypto_app.conexion_bbdd import *
+#from datetime import date
+
+
+
+'''
+- PROBANDO LLAMADA A API -
+time = date.today() # Cómo obtengo esto en el form?
+base = "EUR"
+quote = "BTC"
+API_KEY = "8B30787E-E140-44C4-B21A-E093FB2DAE75"
+
+r = requests.get(f'https://rest.coinapi.io/v1/exchangerate/{base}/{quote}?time={time}&apikey={API_KEY}')
+coin_data = r.json()
+
+if r.status_code == 200:
+    print(coin_data['asset_id_base'])
+    print(coin_data['asset_id_quote'])
+    print(coin_data['time'])
+    print(coin_data['rate'])
+else: 
+    raise Exception( f'Call assets failed:{r.status_code}')
+'''
+
+
+class ModelError(Exception):
+    pass
+
+
+class CoinsApi:
+    def __init__(self):
+        self.base = None
+        self.quote = None
+        self.time = None
+        self.rate = None
+        self.r = None
+
+    def coinData(self, apiKey):
+        self.r = requests.get(f'https://rest.coinapi.io/v1/exchangerate/{self.base}/{self.quote}?time={self.time}&apikey={apiKey}')
+        coin_data = self.r.json()
+
+        if self.r.status_code == 200:
+            self.base = coin_data['asset_id_base']
+            self.quote = coin_data['asset_id_quote']
+            self.time = coin_data['time']
+            self.rate = coin_data['rate']
+        else:
+            raise ModelError( f'Call assets failed:{self.r.status_code}')
 
--- crypto_app/routes.py
+++ crypto_app/routes.py
@@ -1,5 +1,9 @@
 from crypto_app import app
-from flask import render_template
+from flask import render_template, request
+from config import *
+from crypto_app.models import CoinsApi
+from datetime import date
+
 
 @app.route("/")
 def index():
@@ -16,9 +20,21 @@ def index():
 
     return render_template("index.html", data = data_inputs)
 
-@app.route("/purchase")
+@app.route("/purchase", methods = ["GET", "POST"])
 def purchase():
     return render_template("purchase.html")
+    
+    '''
+   #infoCoin = CoinsApi()
+
+    if request.method == "GET":
+        return render_template("purchase.html", data_form = None)
+    
+    else:
+        #Aquí iría lla llamada a la API con los datos que introduzco en el form
+        return render_template("purchase.html")
+        #infoCoin.coinData(API_KEY)
+    '''
 
 @app.route("/status")
 def status():
